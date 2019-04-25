d05c5de16989302350a7d9738a086e545465dba8
Marissa Perry and Tessa Sullivan <tessa@>
2019-04-25 10:18:22 -0700

    Added method to lookup letter score along with tests

--- Scrabble.Tests/ModelTests/ScrabbleTests.cs
+++ Scrabble.Tests/ModelTests/ScrabbleTests.cs
@@ -20,5 +20,32 @@ namespace Scrabble.Tests
         string result = newScrabble.GetWord();
         Assert.AreEqual(word, result);
       }
+      [TestMethod]
+      public void SetWord_SetWord_String()
+      {
+          string word = "Yay";
+          Scrabble newScrabble = new Scrabble(word);
+          string updatedWord = "Wow";
+          newScrabble.SetWord(updatedWord);
+          string result = newScrabble.GetWord();
+          Assert.AreEqual(updatedWord, result);
+      }
+      [TestMethod]
+      public void LookupLetterScore_Integer()
+      {
+        char letter = 'A';
+        string word = "a";
+        int expectedResult = 1;
+        Scrabble newScrabble = new Scrabble(word);
+        Assert.AreEqual(expectedResult, newScrabble.LookupLetterScore(letter));
+      }
+    //   [TestMethod]
+    //   public void GetScore_SingleLetterWord_1()
+    //   {
+    //     string word = "A";
+    //     int expectedResult = 1;
+    //     Scrabble newScrabble = new Scrabble(word);
+    //     Assert.AreEqual(expectedResult, newScrabble.GetScore(word));
+    //   }
   }
 }
--- Scrabble/Models/Scrabble.cs
+++ Scrabble/Models/Scrabble.cs
@@ -12,5 +12,50 @@ namespace Scrabble
         {
             return _word;
         }
+
+        public void SetWord(string newWord)
+        {
+            _word = newWord;
+        }
+        public int GetScore(string word)
+        {
+            return 0;
+        }
+        public int LookupLetterScore(char letter)
+        {
+            if ("AEIOULNRST".Contains(letter))
+            {
+                 return 1;
+            }
+            else if ("DG".Contains(letter))
+            {
+                return 2;
+            }
+            else if ("BCMP".Contains(letter))
+            {
+                return 3;
+            }
+            else if ("FHVWY".Contains(letter))
+            {
+                return 4;
+            }
+            else if ("K".Contains(letter))
+            {
+                return 5;
+            }
+            else if ("JX".Contains(letter))
+            {
+                return 8;
+            }
+            else if ("QZ".Contains(letter))
+            {
+                return 10;
+            }
+            else
+            {
+                return 0;
+            }
+            
+        }
     }
 }
